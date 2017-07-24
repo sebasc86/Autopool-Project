4df90440d9a539e1e627fb56190ccf0b51c77c16
Sebastian <sebascoscia@gmail.com>
2017-07-24 20:59:40 -0300

    cambios

--- autopool/app/User.php
+++ autopool/app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\MyResetPassword;
 
 class User extends Authenticatable
 {
@@ -26,4 +27,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new MyResetPassword($token));
+    }
+
 }
